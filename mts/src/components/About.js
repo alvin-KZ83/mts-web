a226fa3d63939db2e8b6cbd715ed74f8c5ecfecd
alvin <alvin.kz18@gmail.com>
2024-01-05 08:27:57 -0500

    update coloring of SPRING

--- mts/src/components/About.js
+++ mts/src/components/About.js
@@ -1,6 +1,15 @@
 // src/components/About.js
 
 const About = () => {
+    const values = [
+        { colorClass: "bg-blue-500", backgroundClass: "bg-blue-300", textColorClass: "bg-blue-100", letter: "S", description: "TRIVE FOR EXCELLENCE", info: "Constantly seeking to surpass our standards of quality." },
+        { colorClass: "bg-green-500", backgroundClass: "bg-green-300", textColorClass: "bg-green-100", letter: "P", description: "ROFESSIONALISM", info: "Upholding the highest standards in our work and communications." },
+        { colorClass: "bg-yellow-500", backgroundClass: "bg-yellow-300", textColorClass: "bg-yellow-100", letter: "R", description: "ESPECT AND TEAMWORK", info: "Fostering a collaborative environment with mutual respect." },
+        { colorClass: "bg-red-500", backgroundClass: "bg-red-300", textColorClass: "bg-red-100", letter: "I", description: "NTEGRITY", info: "Conducting ourselves with honesty and honor in all situations." },
+        { colorClass: "bg-purple-500", backgroundClass: "bg-purple-300", textColorClass: "bg-purple-100", letter: "N", description: "O DEFECTS", info: "Committing to the highest quality standards and flawless execution." },
+        { colorClass: "bg-indigo-500", backgroundClass: "bg-indigo-300", textColorClass: "bg-indigo-100", letter: "G", description: "O! GO! GO TO THE GOAL!", info: "Focused and driven to achieve our objectives efficiently." }
+    ];
+
     return (
         <div className="container my-auto mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl font-bold text-center mb-8">About Us</h2>
@@ -34,26 +43,19 @@ const About = () => {
                 <h3 className="text-2xl font-semibold mb-4">Our Values</h3>
                 <table className="table-fixed w-full">
                     <tbody>
-                        {[
-                            { color: "blue",   letter: "S", description: "TRIVE FOR EXCELLENCE", info: "Constantly seeking to surpass our standards of quality." },
-                            { color: "green",  letter: "P", description: "ROFESSIONALISM", info: "Upholding the highest standards in our work and communications." },
-                            { color: "yellow", letter: "R", description: "ESPECT AND TEAMWORK", info: "Fostering a collaborative environment with mutual respect." },
-                            { color: "red",    letter: "I", description: "NTEGRITY", info: "Conducting ourselves with honesty and honor in all situations." },
-                            { color: "purple", letter: "N", description: "O DEFECTS", info: "Committing to the highest quality standards and flawless execution." },
-                            { color: "indigo", letter: "G", description: "O! GO! GO TO THE GOAL!", info: "Focused and driven to achieve our objectives efficiently." }
-                        ].map((value, index) => (
+                        {values.map((value, index) => (
                             <tr key={index}>
-                                <td className={`bg-${value.color}-500 text-white p-2 text-center font-semibold w-12 h-12 align-middle`}>{value.letter}</td>
-                                <td className={`p-2 bg-${value.color}-100 text-gray font-semibold align-middle`}>{value.description}</td>
-                                <td className={`p-2 bg-${value.color}-400 text-right font-light`}>{value.info}</td>
+                                <td className={`${value.colorClass} text-white p-2 text-center font-semibold w-12 h-12 align-middle`}>{value.letter}</td>
+                                <td className={`${value.backgroundClass} p-2 font-semibold align-middle`}>{value.description}</td>
+                                <td className={`${value.textColorClass} p-2 text-right font-light`}>{value.info}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
-
         </div>
     );
 };
 
 export default About;
+
